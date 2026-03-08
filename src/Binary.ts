72ff44bd20ef77aaa1f1196bfa26fc89d7ddfd0c
John Hooks <bitmachina@outlook.com>
2026-03-08 08:07:51 -0700

    feat: use dataview

--- src/Binary.ts
+++ src/Binary.ts
@@ -1,115 +1,127 @@
-import type { NumberType } from './types.d';
+import type { NumberType } from './NumberType';
 
 /**
- * A Node.js Buffer Wrapper.
+ * An ArrayBuffer wrapper.
  * Intended for serializing/de-serializing data using a predefined schema.
  */
 export class Binary {
-  private _buffer: Buffer;
-
-  private _byteOffset: number;
+  #buffer: ArrayBuffer;
+  #view: DataView;
+  #byteOffset: number;
 
   /**
-   * @param buffer Either a Buffer or the byte length with which to create a buffer.
-   * @param byteOffset Optionally, if a buffer is provided, initialize `Binary#_byteOffset`
-   * to `byteOffset`.
+   * @param buffer Either an ArrayBuffer or the byte length with which to create one.
+   * @param byteOffset Optionally, if an ArrayBuffer is provided, initialize the view
+   * starting at `byteOffset`.
    */
-  constructor(buffer: Buffer | number, byteOffset = 0) {
-    if (buffer instanceof Buffer) {
-      if (!(byteOffset < buffer.length) || byteOffset < 0) {
-        throw new RangeError('Provide byteOffset is not within the range of the buffer');
+  constructor(buffer: ArrayBuffer | number, byteOffset = 0) {
+    if (buffer instanceof ArrayBuffer) {
+      if (byteOffset < 0 || byteOffset >= buffer.byteLength) {
+        throw new RangeError('Provided byteOffset is not within the range of the buffer');
       }
-      this._buffer = buffer.slice(byteOffset, buffer.length); // Create a new view
-      this._byteOffset = 0;
+      this.#buffer = buffer;
+      this.#view = new DataView(buffer, byteOffset);
+      this.#byteOffset = 0;
     } else {
-      this._buffer = Buffer.alloc(buffer as number);
-      this._byteOffset = 0;
+      this.#buffer = new ArrayBuffer(buffer);
+      this.#view = new DataView(this.#buffer);
+      this.#byteOffset = 0;
     }
   }
 
   /**
-   * I am unsure whether or not this is useful.
    * @returns The length of the wrapped buffer.
    */
   get length(): number {
-    return this._buffer.length;
+    return this.#view.byteLength;
   }
 
   /**
    * @returns The current offset in the wrapped buffer.
    */
   get byteOffset(): number {
-    return this._byteOffset;
+    return this.#byteOffset;
   }
 
   /**
    * Set the current offset to `value`.
    */
   set byteOffset(value: number) {
-    this._byteOffset = value;
-    if (!(this._byteOffset < this._buffer.length) || this._byteOffset < 0) {
+    if (value < 0 || value >= this.#view.byteLength) {
       throw new RangeError('Attempt to seek beyond buffer range');
     }
+    this.#byteOffset = value;
   }
 
   /**
-   * @returns The raw buffer.
+   * @returns The underlying ArrayBuffer.
    */
-  get raw(): Buffer {
-    return this._buffer;
+  get raw(): ArrayBuffer {
+    return this.#buffer;
   }
 
   /**
-   * @returns A new Buffer view from the wrapped buffer, from 0 to the current offset.
+   * @returns A new ArrayBuffer containing bytes from 0 to the current offset.
    */
-  toBuffer(): Buffer {
-    return this._buffer.slice(0, this._byteOffset);
+  toArrayBuffer(): ArrayBuffer {
+    return this.#buffer.slice(0, this.#byteOffset);
+  }
+
+  /**
+   * @returns A Uint8Array view from 0 to the current offset.
+   */
+  toUint8Array(): Uint8Array {
+    return new Uint8Array(this.#buffer, 0, this.#byteOffset);
   }
 
   /**
    * @param start The position to begin the slice. Default is 0.
-   * @param end The position to end the slice (not inclusive). Default is the wrapped
-   * buffer's length.
-   * @returns A new Binary view of the wrapped buffer.
+   * @param end The position to end the slice (not inclusive). Default is the
+   * wrapped buffer's length.
+   * @returns A new Binary wrapping a copy of the sliced region.
    */
-  slice(start = 0, end = this._buffer.length): Binary {
-    return new Binary(this._buffer.slice(start, end));
+  slice(start = 0, end = this.#view.byteLength): Binary {
+    return new Binary(this.#buffer.slice(start, end));
   }
 
   /**
-   * Insert the contents of another buffer into the wrapped buffer starting at the current offset.
-   * @param source The buffer from which to copy.
+   * Insert the contents of a Uint8Array into the wrapped buffer starting at the current offset.
+   * @param source The source to copy from.
    * @param sourceStart The offset within `source` from which to begin copying.
    * @param sourceEnd The offset within `source` from which to stop copying (not inclusive).
    * @param jump Whether or not to adjust the offset to the end position of the insert.
    * @returns The end position of the insert.
    */
-  insert(source: Buffer, sourceStart: number, sourceEnd: number, jump = false): number {
-    const byteCount = source.copy(this._buffer, this._byteOffset, sourceStart, sourceEnd);
+  insert(source: Uint8Array, sourceStart: number, sourceEnd: number, jump = false): number {
+    const target = new Uint8Array(this.#buffer);
+    const chunk = source.subarray(sourceStart, sourceEnd);
+    target.set(chunk, this.#byteOffset);
+    const byteCount = chunk.byteLength;
     if (jump) {
-      this.byteOffset += byteCount;
-      return this.byteOffset;
+      this.#byteOffset += byteCount;
+      return this.#byteOffset;
     }
-    return this.byteOffset + byteCount;
+    return this.#byteOffset + byteCount;
   }
 
   /**
-   * @param type The [[NumberType]] of `value`.
-   * @param value The value to be written to the buffer
-   * @returns The current offset of the buffer after the write
+   * @param type The NumberType of `value`.
+   * @param value The value to be written to the buffer.
+   * @returns The current offset of the buffer after the write.
    */
   write(type: NumberType, value: number): number {
-    this._byteOffset = type.write.call(this._buffer, value, this._byteOffset);
-    return this._byteOffset;
+    type.write(this.#view, value, this.#byteOffset);
+    this.#byteOffset += type.byteLength;
+    return this.#byteOffset;
   }
 
   /**
-   * @param type The [[NumberType]] to read from the buffer.
+   * @param type The NumberType to read from the buffer.
    * @returns The value read from the buffer.
    */
   read(type: NumberType): number {
-    const value = type.read.call(this._buffer, this._byteOffset);
-    this._byteOffset += type.byteLength;
+    const value = type.read(this.#view, this.#byteOffset);
+    this.#byteOffset += type.byteLength;
     return value;
   }
 }
